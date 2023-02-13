3239af52ba5b24d14ec2cf2fa5da94afc5a379be
Elizabeth Karr <e.karr93@icloud.com>
2023-02-13 15:40:29 -0600

    experiementing with event listeners

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ paragraph2.textContent = "ME TOO!";
 div.append(header1, paragraph2);
 
 container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+
+btn.onclick = () => alert("Hello World");
+
+btn.addEventListener('click', () => {
+    alert("Hello World");
+});
+
+btn.addEventListener('click', function (e) {
+    console.log(e);
+});
+
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+});
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
