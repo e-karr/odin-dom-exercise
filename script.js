019d4a80938c114364d1abf7f184f1f376368bd9
Elizabeth Karr <e.karr93@icloud.com>
2023-02-13 15:03:46 -0600

    odin project dom exercise

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+
+const paragraph = document.createElement('p');
+
+paragraph.textContent = "Hey I'm red!";
+paragraph.style.color = 'red';
+
+container.appendChild(paragraph);
+
+const header3 = document.createElement('h3');
+
+header3.textContent = "I'm a blue h3!";
+header3.style.color = 'blue';
+
+container.appendChild(header3);
+
+const div = document.createElement('div');
+
+div.setAttribute('style', 'border: solid black; background-color: pink');
+
+const header1 = document.createElement('h1');
+
+header1.textContent = "I'm in a div";
+
+const paragraph2 = document.createElement('p');
+
+paragraph2.textContent = "ME TOO!";
+
+div.append(header1, paragraph2);
+
+container.appendChild(div);
